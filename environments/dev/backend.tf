90bb5565e154628d95b8ed5c20d12b348d099053
ganeshnadar92 <gan1234568@gmail.com>
2021-04-25 13:59:25 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "neon-polymer-366-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="neon-polymer-366"
